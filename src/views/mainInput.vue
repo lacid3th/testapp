eb1349fc913ad9649a72a17af7fc6963481ebc6e
jay <lacid3th@gmail.com>
2023-04-14 10:31:34 +0900

    kim

--- src/views/mainInput.vue
+++ src/views/mainInput.vue
@@ -22,15 +22,43 @@
           <v-btn class="mr-3" @click="columnControl"> Del Column </v-btn>
           <v-btn class="mr-3" @click="serverSend"> Send Serve </v-btn>
           <v-btn class="mr-3"> Button </v-btn>
-          <v-btn class="mr-3"> Button </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div class="text-center">
+            <v-select
+              v-model="selectedManager"
+              :items="managerlist"
+              label="USER"
+            ></v-select>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="text-center">
+            <v-select
+              v-model="selectedSeller"
+              :items="sellerlist"
+              label="SELLER"
+            ></v-select>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="text-center">
+            <v-select
+              v-model="selectedBrand"
+              :items="brandlist"
+              label="BRAND"
+            ></v-select>
+          </div>
+        </v-col>
+        <v-col>
+          <v-btn class="mr-3 w-100" @click="SBMApply"> APPLY </v-btn>
         </v-col>
       </v-row>
     </v-container>
     <div>
-      <v-table style="font-family: system-ui, 'Segoe UI"
-         fixed-header
-         height="1000px
-         " class="text-left" density="compact">
+      <v-table class="text-left" density="compact">
         <thead>
           <tr>
             <th v-for="header in headers" :key="header">
@@ -38,138 +66,16 @@
             </th>
           </tr>
         </thead>
-        <tbody>
+        <tbody >
           <tr v-for="(line, i) in data" :key="i">
-            <td v-for="(row, j) in data[0]" :key="j">
+            <td style="font-size:auto" v-for="(row, j) in data[0]" :key="j">
               {{ data[i][j] }}
             </td>
           </tr>
         </tbody>
       </v-table>
     </div>
-
   </div>
-
-  <!-- <div>
-    <div
-      class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
-    >
-      <h1 class="h2">Functions</h1>
-
-      <div class="btn-toolbar mb-2 mb-md-0">
-        <div class="btn-group me-2">
-          <input
-            type="file"
-            class="btn btn-sm btn-outline-secondary"
-            v-on:change="addfile($event)"
-            placeholder="Upload file"
-            accept=".csv,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-          />
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            @click="columnControl"
-          >
-            Apply
-          </button>
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            @click="serverSend"
-          >
-            Server Send
-          </button>
-        </div>
-      </div>
-    </div>
-    <div
-      style="display: block; justify-items: flex-start"
-      class="d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom gap-2"
-    >
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton1"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Seller : {{ selectedSeller }}
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li v-for="(item, i) in sellerlist" :key="i">
-            <a class="dropdown-item" href="#" @click="sellerNameApply(i)">{{
-              item
-            }}</a>
-          </li>
-        </ul>
-      </div>
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton2"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Brand : {{ selectedBrand }}
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li v-for="(item, i) in brandlist" :key="i">
-            <a class="dropdown-item" href="#" @click="brandNameApply(i)">{{
-              item
-            }}</a>
-          </li>
-        </ul>
-      </div>
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton3"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Manager : {{ selectedManager }}
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li v-for="(item, i) in managerlist" :key="i">
-            <a class="dropdown-item" href="#" @click="managerNameApply(i)">{{
-              item
-            }}</a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          @click="SBMApply"
-        >
-          Apply
-        </button>
-      </div>
-    </div>
-    <h2>Order List</h2>
-    <div>
-      <table class="table table-striped table-sm" style="">
-        <thead>
-          <tr>
-            <th v-for="header in headers" :key="header">
-              {{ header }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(line, i) in data" :key="i">
-            <td v-for="(row, j) in data[0]" :key="j">
-              {{ data[i][j] }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div> -->
 </template>
 
 
@@ -193,8 +99,9 @@ export default {
       selectedSeller: null,
       brandlist: ["애즈유", "침대팔이", "XYZ"],
       selectedBrand: null,
-      managerlist: ["유지영", "송유진", "윤혜미"],
+      managerlist: [],
       selectedManager: null,
+      loadedData: null,
     };
   },
   methods: {
@@ -216,7 +123,7 @@ export default {
         var worksheet = workbook.Sheets[first_sheet_name];
         // console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
         this.arraylist = XLSX.utils.sheet_to_json(worksheet, {
-          header: 1,
+          header : 1,
           blankrows: false,
         });
         // { header : 1 옵션 넣으면 json 아니고 그냥 array로 나옴}
@@ -224,17 +131,15 @@ export default {
 
         console.log(this.arraylist);
 
-        // HTML 에 쏘는 파트
+        // OBJ 형식 Array 로 바꿔서 HTML 전송
         this.headers = my.array2Table(this.arraylist)[0];
         this.data = my.array2Table(this.arraylist)[1];
 
         // Array 수정 파트
 
-        // 수정된 Array를 Json 형식의 object 로 만듬 stringlify 는 전송 안됨
         this.jsonlist = my.array2Json(this.arraylist);
 
         console.log(this.jsonlist);
-        // return this.setup(); // 순차적으로 Setup 함수 실행
       };
     },
     columnControl() {
@@ -243,45 +148,6 @@ export default {
       this.data = my.array2Table(this.arraylist)[1];
     },
 
-    // setup() {
-    //   var data = this.arraylist;
-    //   const getHeaders = () => {
-    //     let h = [];
-    //     if (data.length > 0) {
-    //       var columnsIn = data[0];
-    //       for (var key in columnsIn) {
-    //         h.push({ text: key });
-    //         // console.log(key); // here is your column name you are looking for
-    //       }
-    //     } else {
-    //       console.log("No columns");
-    //     }
-    //     return h;
-    //   };
-    //   // let headers = ref([{text : 'col-1'},{text : 'col-2'}])
-    //   var headers = getHeaders();
-    //   // return {
-    //   //   data,
-    //   //   headers
-    //   // }
-
-    //   for (let index = 0; index < data.length; index++) {
-    //     data[index].Seller = "";
-    //     data[index].Brand = "";
-    //     data[index].Manager = "";
-    //   }
-
-    //   this.data = data;
-    //   headers = headers.concat(
-    //     { text: "Seller" },
-    //     { text: "Brand" },
-    //     { text: "Manager" }
-    //   );
-    //   this.headers = headers;
-
-    //   // console.log(this.data);
-    //   // console.log(headers);
-    // },
     SBMApply() {
       // for (let index = 0; index < this.data.length; index++) {
       //   this.data[index].Seller = this.selectedSeller;
@@ -330,6 +196,24 @@ export default {
     managerNameApply(i) {
       this.selectedManager = this.managerlist[i];
     },
+
+    async loadUserData_FromServer() {
+      // 조건 붙여서 리퀘스트 하기
+      await axios.post("http://localhost:8000/loadUserInfo", "dummy").then(
+        (response) => {
+          // console.log(response.data);
+          this.loadedData = response.data;
+          console.log(this.loadedData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      this.managerlist = this.loadedData.map(row=>row.Name);
+    },
+  },
+  mounted() {
+    this.loadUserData_FromServer();
   },
 };
 </script>
--- src/views/registerTree/registerUser.vue
+++ src/views/registerTree/registerUser.vue
@@ -1,121 +1,150 @@
 <template>
+  <div>
+    <p
+      class="pa-5"
+      style="
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+      "
+    >
+      USER
+    </p>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="userName"
+              label="Name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col>
+            <v-text-field
+              v-model="userAge"
+              label="Age"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col>
+            <v-text-field
+              v-model="userID"
+              label="ID"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col class="m-2">
+            <v-btn class="mr-3" @click="save2Server"> APPLY </v-btn>
+            <v-btn class="mr-3" @click="delFromServer"> DELETE </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    
     <div>
-        <div
-            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h4 class="h2">USER Register</h4>
-        </div>
-        <div style="display: block; justify-items: flex-start"
-            class="d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom gap-2">
-            <input v-model="userName" class="form-control" type="text" placeholder="Name"
-                aria-label="default input example">
-            <input v-model="userAge" class="form-control" type="text" placeholder="Age" aria-label="default input example">
-            <input v-model="userID" class="form-control" type="text" placeholder="ID" aria-label="default input example">
-            <div>
-                <button type="button" class="btn btn-outline-secondary" @click="save2Server">
-                    Save to DB
-                </button>
-                <button type="button" class="btn btn-outline-secondary" @click="delFromServer">
-                    Del from DB
-                </button>
-            </div>
-        </div>
-        <div>
-            <table class="table table-striped table-sm" style="">
-                <thead>
-                    <!-- 이거 있어야 스타일 제대로 먹음 -->
-                    <tr>
-                        <th v-for="header in headers" :key="header">
-                            {{ header }}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(line, i) in data" :key="i">
-                        <td v-for="(row, j) in data[0]" :key="j">
-                            {{ data[i][j] }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      <v-table class="" style="">
+        <thead>
+          <tr>
+            <th v-for="header in headers" :key="header">
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(line, i) in data" :key="i">
+            <td v-for="(row, j) in data[0]" :key="j">
+              {{ data[i][j] }}
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
     </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
-import * as my from '../../myFunction.js';
+import * as my from "../../myFunction.js";
 
 export default {
-    data() {
-        return {
-            userName: null,
-            userAge: null,
-            userID: null,
-            data: null,
-            headers: null,
-            loadedData: null,
-            loadedArray: null,
-        }
-    },
-    methods: {
+  data() {
+    return {
+      userName: null,
+      userAge: null,
+      userID: null,
+
+      data: null,
+      headers: null,
+      loadedData: null,
+    };
+  },
+  methods: {
         async loadFromServer() {
-            // 조건 붙여서 리퀘스트 하기
-            await axios.post("http://localhost:8000/loadUserInfo", "dummy").then(
-                (response) => {
-                    // console.log(response.data);
-                    this.loadedData = response.data;
-                    console.log(this.loadedData)
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-            this.loadedArray = my.obj2Array(this.loadedData)
-            this.headers = my.array2Table(this.loadedArray)[0];
-            this.data = my.array2Table(this.loadedArray)[1];
-        },
-        async save2Server() {
-            var c = confirm("이름 :" + this.userName + '\n'
-                          + "나이 :" + this.userAge + '\n'
-                          + "ID :" + this.userID + '\n'
-                          + "로 입력하시겠습니까?");
-            if(c == true){
-                let userInfo = {
-                Name: this.userName,
-                Age: this.userAge,
-                ID: this.userID,
-            };
+        // 조건 붙여서 리퀘스트 하기
+        await axios.post("http://localhost:8000/loadUserInfo", "dummy").then(
+            (response) => {
+            // console.log(response.data);
+            this.loadedData = response.data;
+            console.log(this.loadedData);
+            },
+            (error) => {
+            console.log(error);
+        }
+      );
+        this.headers = my.obj2Array_for_Table(this.loadedData)[0];
+        this.data = my.obj2Array_for_Table(this.loadedData)[1];
 
-            await axios
-                .post("http://localhost:8000/saveUser", userInfo)
-                .then(function (result) {
-                    console.log(result.data);
-                });
-                // setTimeout 쓸때 매개변수 있으면 조심해야 함, 뒤쪽에 넣어야 됨
-            setTimeout(this.loadFromServer,1000);
-            }
-            else {
-                alert("취소되었습니다");
-            }
-        },
-
-        async delFromServer() {
-            let userName = {
-                Name: this.userName,
-            };
-            await axios
-                .post("http://localhost:8000/delUser", userName)
-                .then(function (result) {
-                    console.log(result.data);
-                });
-            this.loadFromServer();
-        },
     },
-    // 여기 있으면 페이지 불러오자마자 실행됨
-    mounted() {
-        this.loadFromServer();
-    },
-}
+    async save2Server() {
+      var c = confirm(
+        "이름 :" +
+          this.userName +
+          "\n" +
+          "나이 :" +
+          this.userAge +
+          "\n" +
+          "ID :" +
+          this.userID +
+          "\n" +
+          "로 입력하시겠습니까?"
+      );
+      if (c == true) {
+        let userInfo = {
+          Name: this.userName,
+          Age: this.userAge,
+          ID: this.userID,
+        };
 
+        await axios
+          .post("http://localhost:8000/saveUser", userInfo)
+          .then(function (result) {
+            console.log(result.data);
+          });
+        // setTimeout 쓸때 매개변수 있으면 조심해야 함, 뒤쪽에 넣어야 됨
+        setTimeout(this.loadFromServer, 1000);
+      } else {
+        alert("취소되었습니다");
+      }
+    },
+
+    async delFromServer() {
+      let userName = {
+        Name: this.userName,
+      };
+      await axios
+        .post("http://localhost:8000/delUser", userName)
+        .then(function (result) {
+          console.log(result.data);
+        });
+      this.loadFromServer();
+    },
+  },
+  // 여기 있으면 페이지 불러오자마자 실행됨
+  mounted() {
+    this.loadFromServer();
+  },
+};
 </script>
 
 <style></style>
